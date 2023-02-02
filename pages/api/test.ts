04c90c3423a8ab4b2af694996291182078265b3b
Ryan Killeen <ryankilleen@Ryans-MacBook-Pro.local>
2023-02-02 12:37:25 -0500

    parallelize playwright contexts

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -1,20 +1,28 @@
 import playwright from "playwright";
 import { writeFileSync } from "fs";
-
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { performance, PerformanceObserver } from "perf_hooks";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { PDFDocument } from "pdf-lib";
 
 type Data = Buffer;
 
-const getPageAsPDFDocument = async (url: string) => {
-  const browser = await playwright.chromium.launch({ headless: true });
-  const context = await browser.newContext();
-  const page = await context.newPage();
-  await page.goto(url);
-  const file = await page.pdf({ path: `document.pdf` });
-  await browser.close();
-  return await PDFDocument.load(file);
+const getPagesAsPDFDocuments = async (urls: string[]) => {
+  const pdfs: Promise<PDFDocument>[] = [];
+  const generatePDF = async (url: string) => {
+    const browser = await playwright.chromium.launch({ headless: true });
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto(url);
+    const file = await page.pdf({ path: `document.pdf` });
+    await browser.close();
+    return await PDFDocument.load(file);
+  };
+
+  for (const url of urls) {
+    pdfs.push(generatePDF(url))
+  }
+
+  return Promise.all(pdfs);
 };
 
 const mergePDFs = async (pdfs: PDFDocument[]) => {
@@ -29,18 +37,27 @@ const mergePDFs = async (pdfs: PDFDocument[]) => {
 };
 
 const handler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
-  const pdfA = await getPageAsPDFDocument(
-    "https://www.google.com/search?q=Google"
+  const observer = new PerformanceObserver((list) =>
+    list.getEntries().forEach((entry) => console.info(entry))
   );
-  const pdfB = await getPageAsPDFDocument(
-    "https://www.google.com/search?q=pdf-lib"
-  );
-  const resultFile = await mergePDFs([pdfA, pdfB]);
+  observer.observe({ buffered: true, entryTypes: ["measure"] });
+  performance.mark("start");
 
+  const resultFile = await mergePDFs(
+    await getPagesAsPDFDocuments([
+      "https://www.google.com/search?q=Google",
+      "https://www.google.com/search?q=pdf-lib",
+      "https://www.google.com/search?q=dogs",
+    ])
+  );
+
+  performance.mark("stop");
+  performance.measure("Business Logic", "start", "stop");
   writeFileSync("test.pdf", resultFile);
   const toServe = Buffer.from(resultFile);
   res.setHeader("Content-Type", "application/pdf");
   res.setHeader("Content-Disposition", 'attachment; filename="test.pdf"');
   res.send(toServe);
 };
+
 export default handler;
